26c919f7f6fc0ef14e46036f81b3bdcf99e5f0d7
Sameer Singh <ssg8719@gmail.com>
2023-08-14 19:44:56 +0530

    done

--- invoice/models.py
+++ invoice/models.py
@@ -12,8 +12,8 @@ class Invoice(models.Model):
 
 
 class InvoiceDetail(models.Model):
-    invoice_number = models.OneToOneField(Invoice, on_delete=models.CASCADE,related_name="invoice_detail")
+    invoice_number = models.OneToOneField(Invoice, on_delete=models.CASCADE, related_name="invoice_detail",editable=False)
     description = models.TextField()
     quantity = models.IntegerField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
-    price = models.DecimalField(max_digits=10, decimal_places=2,editable=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
