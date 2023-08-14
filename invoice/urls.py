320117b174a95c8d3274627f9c0437c2b63ff85d
Sameer Singh <ssg8719@gmail.com>
2023-08-14 18:14:53 +0530

    done

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from invoice.views import invoice
+from invoice.views import invoice, invoice_details
 
 urlpatterns = [
     path('', invoice, name=""),
+    path('detail/', invoice_details, name="invoice_details"),
 ]
