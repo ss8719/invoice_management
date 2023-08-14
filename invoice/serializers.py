78e4ddfcb7df46f303ee82ed670a721c293026a5
Sameer Singh <ssg8719@gmail.com>
2023-08-14 18:05:40 +0530

    done

--- invoice/serializers.py
+++ invoice/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from invoice.models import Invoice
+from invoice.models import Invoice, InvoiceDetail
 
 
 class InvoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Invoice
+        fields = "__all__"
+
+
+class InvoiceDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InvoiceDetail
         fields = "__all__"
