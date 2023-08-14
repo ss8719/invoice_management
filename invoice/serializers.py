9b41aefbf38376ef6cffdc3c5bfbc99a98ed8f25
Sameer Singh <ssg8719@gmail.com>
2023-08-14 19:35:07 +0530

    done

--- invoice/serializers.py
+++ invoice/serializers.py
@@ -19,6 +19,5 @@ class InvoiceDetailSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        print(validated_data)
         validated_data["price"] = validated_data["unit_price"] * validated_data["quantity"]
         return InvoiceDetail.objects.create(**validated_data)
