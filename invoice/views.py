ea3479562dd34f795ead536238b8f5f15c696900
Sameer Singh <ssg8719@gmail.com>
2023-08-14 18:11:54 +0530

    done

--- invoice/views.py
+++ invoice/views.py
@@ -1,5 +1,9 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from invoice.models import Invoice
+from invoice.serializers import InvoiceSerializer
 
 
 # Create your views here.
@@ -7,6 +11,8 @@ from rest_framework.response import Response
 @api_view(["GET", "POST"])
 def invoice(request):
     if request.method == "GET":
-        return Response("lkdjflakdjf")
+        all_invoices = Invoice.objects.all()
+        serializer = InvoiceSerializer(all_invoices, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "POST":
         return Response("lkdjflakdjf")
