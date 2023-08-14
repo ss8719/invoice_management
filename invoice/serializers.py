56027c8520c3bcfb2d04462e11b200365b88336e
Sameer Singh <ssg8719@gmail.com>
2023-08-14 18:43:57 +0530

    done

---
+++ invoice/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from invoice.models import Invoice, InvoiceDetail
+
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    # invoice_detail = InvoiceDetailSerializer(read_only=True)
+
+    class Meta:
+        model = Invoice
+        fields = "__all__"
+class InvoiceDetailSerializer(serializers.ModelSerializer):
+    invoice_number = InvoiceSerializer()
+
+    class Meta:
+        model = InvoiceDetail
+        fields = "__all__"
+
+    def create(self, validated_data):
+        validated_data["price"] = validated_data["unit_price"] * validated_data["quantity"]
+        return InvoiceDetail.objects.create(**validated_data)
